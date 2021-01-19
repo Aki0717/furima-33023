0aec063df7061a870802529ff987a687ae7617a9
Aki0717 <75903070+Aki0717@users.noreply.github.com>
2021-01-19 18:19:42 +0900

    registrations/newファイルのカラム設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-      validates :email, unique: true
+      validates :email, uniqueness: true
       validates :nickname, presence: true
       validates :first_name, presence: true
       validates :last_name, presence: true
 
-      with_option presence: true, format: { with:/\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
+      with_options presence: true, format: { with:/\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
         validates :kana_first_name
         validates :kana_last_name
       end
-      
+
       validates :birth_date, presence: true
+
+      has_many :items
+      has_many :orders
 end
