036b46977eca69e4aa3b97ec48b54581f4c687fc
Aki0717 <75903070+Aki0717@users.noreply.github.com>
2021-01-19 17:09:22 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+      validates :email, unique: true
+      validates :nickname, presence: true
+      validates :first_name, presence: true
+      validates :last_name, presence: true
+
+      with_option presence: true, format: { with:/\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
+        validates :kana_first_name
+        validates :kana_last_name
+      end
+      
+      validates :birth_date, presence: true
 end
