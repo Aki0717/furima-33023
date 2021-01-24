a2c55977daf84ae60f7ac1477a97a3ea13e6f1fc
Aki0717 <75903070+Aki0717@users.noreply.github.com>
2021-01-24 14:29:51 +0900

    テストコード修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -10,30 +10,6 @@ RSpec.describe Item, type: :model do
       it "登録機能全てが記載されていたら登録できる" do
         expect(@item).to be_valid
       end
-      it "category_idが2以上11以下だと登録できる" do
-        @item.category_id = 4
-        expect(@item).to be_valid
-      end
-      it "condition_idが2以上7以下だと登録できる" do
-        @item.condition_id = 5
-        expect(@item).to be_valid
-      end
-      it "delivery_fee_idが2以上3以下だと登録できる" do
-        @item.delivery_fee_id = 5
-        expect(@item).to be_valid
-      end
-      it "prefecture_idが2以上48以下だと登録できる" do
-        @item.prefecture_id = 5
-        expect(@item).to be_valid
-      end
-      it "days_to_ship_idが2以上4以下だと登録できる" do
-        @item.days_to_ship_id = 5
-        expect(@item).to be_valid
-      end
-      it "priceが300以上9999999未満だと登録できる" do
-        @item.price = 500
-        expect(@item).to be_valid
-      end
     end
 
     context "商品の出品ができない" do
