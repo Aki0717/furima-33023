f1a7b5569c4561b6119df63c4e6fb4cbc158e887
Aki0717 <75903070+Aki0717@users.noreply.github.com>
2021-01-25 21:12:23 +0900

    商品情報編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show ]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :baria_user, only: :edit
 
   def index
     @items = Item.all.order(id: "DESC")
@@ -23,7 +24,16 @@ class ItemsController < ApplicationController
   end
 
   def edit
-      
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   def destroy
@@ -34,5 +44,11 @@ class ItemsController < ApplicationController
   private
   def item_params
     params.require(:item).permit(:title, :text, :category_id, :condition_id, :delivery_fee_id, :prefecture_id, :days_to_ship_id, :price, :image).merge(user_id: current_user.id)
+  end
+  
+  def baria_user
+    unless Item.find(params[:id]).user.id.to_i == current_user.id
+      redirect_to root_path(current_user)
+    end
   end
 end
