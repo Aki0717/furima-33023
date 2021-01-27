ca287e24baa49e79a7dfe74022ddb0f09a00c64c
Aki0717 <75903070+Aki0717@users.noreply.github.com>
2021-01-27 11:49:59 +0900

    テーブル保存の実装

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,25 @@
+class OrdersController < ApplicationController
+
+  def index
+    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new
+  end
+
+  def create
+    binding.pry
+    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(:post_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(item_id: params[:item_id], user_id: current_user.id)
+  end
+end
---
+++ app/models/order_address.rb
@@ -0,0 +1,20 @@
+class OrderAddress
+  include ActiveModel::Model
+  attr_accessor :post_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :item_id, :user_id
+
+  with_options presence: true do
+    validates :post_code
+    validates :prefecture_id
+    validates :city
+    validates :house_number
+    validates :phone_number
+  end
+
+  validates :prefecture_id, numericality: { other_than: 1 }
+  validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
+
+  def save
+    order = Order.create(item_id: item_id, user_id: user_id)
+    Address.create!(post_code: post_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: house_number, phone_number: phone_number, order_id: order.id)
+  end
+end
